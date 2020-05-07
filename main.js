0ae648c0ade8fcf647ef57c35c63a174b5fdd689
Patryk Gruszczyński <gruszczynski0888@gmail.com>
2020-05-07 18:52:24 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+alert('cześć witam Cie na mojej stronce:)');
+
+const firstName = 'Patryk';
+const age = '31';
+
+console.log(firstName);
+console.log(age);
+console.log(`Siema, nazywam się ${firstName} i mam ${age} lat`);
